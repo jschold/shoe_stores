7158974f8a02d646124eb4d9d0699fed63c23331
jschold <jscholdny@msn.com>
2015-09-01 20:04:59 -0700

    add tests for store setStoreName, getStoreName, getId, getAll, and save, and add matching functions in source store file.

--- src/Store.php
+++ src/Store.php
@@ -1,3 +1,73 @@
 <?php
 
+    class Store
+    {
+        private $store_name;
+        private $id;
+
+        function __construct($store_name, $id = null)
+        {
+            $this->store_name = $store_name;
+            $this->id = $id;
+        }
+
+        function setStoreName($new_store_name)
+        {
+            $this->store_name = $new_store_name;
+        }
+
+        function getStoreName()
+        {
+            return $this->store_name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO store (store_name) VALUES ('{$this->getStoreName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        // function update()
+        // {
+        //     $GLOBALS['DB']->exec("UPDATE store SET store_name = '{$this->getStoreName()}' WHERE id = {$this->getId()};");
+        // }
+
+        // function deleteOne()
+        // {
+        //     $GLOBALS['DB']->exec("DELETE FROM store WHERE id = {$this->getId()};");
+        // }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM store;");
+        }
+
+        static function getAll()
+        {
+            $stores_returned = $GLOBALS['DB']->query("SELECT * FROM store;");
+            $stores = array();
+            foreach($stores_returned as $store){
+                $store_name = $store['store_name'];
+                $id = $store['id'];
+                $new_store = new Store($store_name, $id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+        }
+
+        // static function find($searchId)
+        // {
+        //     $stores_returned = Store::getAll();
+        //     foreach($stores_returned as $store){
+        //        if ($searchId == $store->getId()){
+        //            return $store;
+        //        }
+        //     }
+        // }
+    }
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -1,3 +1,80 @@
 <?php
+    /**
+    *@backupGlobals disabled
+    *@backupStaticAttributes disabled
+    */
 
+    require_once "src/store.php";
+    require_once "src/brand.php";
+
+    $server = 'mysql:host=localhost;dbname=shoe_stores_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    class CategoryTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Store::deleteAll();
+            Brand::deleteAll();
+        }
+
+        function testSetStoreName()
+        {
+            $store_name = "Beacon's Closet";
+            $new_store = new Store($store_name);
+            $new_store->setStoreName("Buffalo Exchange");
+            $result = $new_store->getStoreName();
+            $this->assertEquals("Buffalo Exchange", $result);
+        }
+
+        function testGetStoreName()
+        {
+            $store_name = "Beacon's Closet";
+            $new_store = new Store($store_name);
+
+            $result = $new_store->getStoreName();
+
+            $this->assertEquals("Beacon's Closet", $result);
+        }
+
+        function testGetId()
+        {
+            $store_name = "Beacon's Closet";
+            $id = 1;
+            $new_store = new Store($store_name, $id);
+
+            $result = $new_store->getId();
+
+            $this->assertEquals(1, $result);
+        }
+
+        function testGetAll()
+        {
+            $store_name = "Beacon's Closet";
+            $new_store = new Store($store_name);
+            $new_store->save();
+
+            $store_name2 = "Buffalo Exchange";
+            $new_store2 = new Store($store_name);
+            $new_store2->save();
+
+            $result = Store::getAll();
+            var_dump($result);
+
+            $this->assertEquals([$new_store, $new_store2], $result);
+        }
+
+        function testSave()
+        {
+            $store_name = "Beacon's Closet";
+            $new_store = new Store($store_name);
+
+            $new_store->save();
+            $result = Store::getAll();
+
+            $this->assertEquals($new_store, $result[0]);
+        }
+
+    }
 ?>
