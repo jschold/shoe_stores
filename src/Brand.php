ca12534e3d91552a9065f43b203ab6db914005b1
jschold <jscholdny@msn.com>
2015-09-01 18:24:55 -0700

    add tests for set and get Brand Name and matching functions in source file.

--- src/Brand.php
+++ src/Brand.php
@@ -1,3 +1,64 @@
 <?php
 
+    class Brand
+    {
+        private $brand_name;
+        private $id;
+
+        function __construct($brand_name, $id = null)
+        {
+            $this->brand_name = $brand_name;
+            $this->id = $id;
+        }
+        function setBrandName($new_brand_name)
+        {
+            $this->brand_name = $new_brand_name;
+        }
+        function getBrandName()
+        {
+            return $this->brand_name;
+        }
+        // function getId()
+        // {
+        //     return $this->id;
+        // }
+        // function save()
+        // {
+        //     $GLOBALS['DB']->exec("INSERT INTO brands (brand_name) VALUES ('{$this->getBrandName()}');");
+        //     $this->id = $GLOBALS['DB']->lastInsertId();
+        // }
+        // function update()
+        // {
+        //     $GLOBALS['DB']->exec("UPDATE brands SET brand_name = '{$this->getBrandName()}' WHERE id = {$this->getId()};");
+        // }
+        // function deleteOne()
+        // {
+        //     $GLOBALS['DB']->exec("DELETE FROM brands WHERE id = {$this->getId()};");
+        // }
+        // static function deleteAll()
+        // {
+        //     $GLOBALS['DB']->exec("DELETE FROM brands;");
+        // }
+        // static function getAll()
+        // {
+        //     $brands_returned = $GLOBALS['DB']->query("SELECT * FROM brands;");
+        //     $brands = array();
+        //     foreach($brands_returned as $brand){
+        //         $id = $brand['id'];
+        //         $brand_name = $brand['brand_name'];
+        //         $new_brand = new Brand($brand_name, $id);
+        //         array_push($brands, $new_brand);
+        //     }
+        //     return $brands;
+        // }
+        // static function find($searchId)
+        // {
+        //     $brands_returned = Brand::getAll();
+        //     foreach($brands_returned as $brand){
+        //        if ($searchId == $brand->getId()){
+        //            return $brand;
+        //        }
+        //     }
+        // }
+    }
 ?>
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -1,3 +1,29 @@
 <?php
+    /**
+    *@backupGlobals disabled
+    *@backupStaticAttributes disabled
+    */
 
+    require_once "src/brand.php";
+
+    $server = 'mysql:host=localhost;dbname=shoe_stores_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    class BrandTest extends PHPUnit_Framework_TestCase
+    {
+        // protected function tearDown()
+        // {
+        //     Brand::deleteAll();
+        // }
+
+        function testSetBrandName()
+        {
+            $brand_name = "dr.martens";
+            $new_brand = new Brand($brand_name);
+            $new_brand->setbrandName("doc martens");
+            $result = $new_brand->getbrandName();
+            $this->assertEquals("doc martens", $result);
+        }
+    }
 ?>
